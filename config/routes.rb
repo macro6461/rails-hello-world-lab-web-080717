e3a397f1bc61da04fa901adb0cb220a3ffec690f
macro6461 <mattcroak718@gmail.com>
2017-09-03 14:59:46 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  get 'hello_world', to: 'static#hello_world'
+
   # Example resource route with options:
   #   resources :products do
   #     member do
